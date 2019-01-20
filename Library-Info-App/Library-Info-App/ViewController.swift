65023549ad798c0efec8698000392f003cf38643
2529716798 <2529716798@qq.com>
2019-01-20 23:44:09 +0800

    init

---
+++ Library-Info-App/Library-Info-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Library-Info-App
+//
+//  Created by py on 2019/1/20.
+//  Copyright © 2019年 NJU.py. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
